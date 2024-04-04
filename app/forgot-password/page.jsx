45867b8e808aff663977d12badd58889906aa694
mayor falomo <mayorfalomo@gmail.com>
2024-04-04 14:19:14 +0100

    changed placeholders for forgotten-password

--- app/forgot-password/page.jsx
+++ app/forgot-password/page.jsx
@@ -35,32 +35,6 @@ const Login = () => {
 
   const router = useRouter();
 
-  // const signInWithGoogle = async (e) => {
-  //   e.preventDefault();
-  //   signInWithPopup(auth, provider)
-  //     .then(async (res) => {
-  //       let userInfo = {
-  //         userId: res.user.uid,
-  //       };
-  //       // console.log(userInfo);
-  //       //   localStorage.setItem("user", userInfo?.userId);
-
-  //       // const response = await axios.get(`https://`);
-  //       router.push("/");
-  //       toast(`${res.user.displayName}, login successful!`);
-
-  //       if (userInfo?.userId) {
-  //         localStorage.setItem("user", userInfo?.userId);
-
-  //         //   userInfo?.userId
-  //         //     ? contextValue?.getCurrentUser(userObject.data?._id)
-  //         //     : "";
-  //         router.push("/");
-  //       }
-  //     })
-  //     .catch((err) => console.log(err));
-  // };
-
   //   const handleLogin = async (e) => {
   //     e.preventDefault();
 
@@ -155,7 +129,6 @@ const Login = () => {
               bgColor="#fff"
               borderRadius="15px"
               boxShadow="rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;"
-              // border="2px red solid"
             >
               <Stack
                 display="flex"
@@ -166,28 +139,17 @@ const Login = () => {
                 spacing="20px"
                 h="100%"
                 className="w-[90%] max-sm:w-[100%] "
-                // boxShadow="rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;"
               >
                 <Heading as="h3" size="lg">
                   Enter your account
                 </Heading>
-                <form
-                  className="flex flex-col gap-6 w-full "
-                  // onSubmit={handleLogin}
-                >
-                  {/* <Input
-                    onChange={(e) => setEmail(e.target.value)}
-                    type="email"
-                    placeholder="Enter email address"
-                    defaultValue={email}
-                    size="lg"
-                  /> */}
+                <form className="flex flex-col gap-6 w-full ">
                   {hidePassword ? (
                     <Box position="relative">
                       <Input
                         onChange={(e) => setPassword(e.target.value)}
                         type="text"
-                        placeholder="Enter old password"
+                        placeholder="Enter new password"
                         size="lg"
                         defaultValue={password}
                       />
@@ -202,7 +164,7 @@ const Login = () => {
                     <Box position="relative">
                       <Input
                         type="password"
-                        placeholder="Enter old password"
+                        placeholder="Confirm password"
                         size="lg"
                         onChange={(e) => setPassword(e.target.value)}
                         defaultValue={password}
@@ -292,37 +254,6 @@ const Login = () => {
                     ""
                   )}
                 </form>
-                {/* <Text fontSize="md">
-              Don't have have an account?{" "}
-              <Link as={NextLink} href="/signup">
-                <span className="text-[teal] cursor-pointer ">Sign up </span>
-              </Link>
-            </Text> */}
-                {/* <Flex alignItems="center" gap="10px">
-                  <span className="h-[1px] w-[200px] bg-black opacity-[0.25]  max-nav:w-[150px] max-sm:w-[100px]  "></span>
-                  <Text fontSize="sm">OR </Text>
-                  <span className="h-[1px] w-[200px] bg-black opacity-[0.25]  max-nav:w-[150px] max-sm:w-[100px]  "></span>
-                </Flex> */}
-                {/* <Text fontSize="md">
-                  Forgot Password?{" "}
-                  <Link as={NextLink} href="#">
-                    <span className="text-[teal] cursor-pointer ">
-                      Click here{" "}
-                    </span>
-                  </Link>
-                </Text> */}
-                {/* <Button
-              display="flex"
-              alignItems="center"
-              //   justifyContent="flex-start"
-              gap="20px"
-              size="lg"
-              w="100%"
-              onClick={signInWithGoogle}
-            >
-              {<FcGoogle className="text-[24px] max-[400px]:text-[18px]" />}
-              <Text fontSize="md">Continue with Google </Text>
-            </Button> */}
               </Stack>
             </Box>
           </Container>
