3d3a1f714482a9633041a1a0707279b584317058
mayor falomo <mayorfalomo@gmail.com>
2024-05-13 07:51:10 +0100

    updated time on nav

--- app/components/Navbar/Navbar.jsx
+++ app/components/Navbar/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
   var now = moment.utc();
 
   // Format the current UTC time in hours, minutes, and seconds
-  var formattedTime = now.format("yy/M/D HH:mm:ss");
+  var formattedTime = now.format("HH:mm:ss - yy/M/D");
 
   return (
     <div
